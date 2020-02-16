aacff0fdbe283c49504b9a7c035b9dd479810d03
Shubhra <ssuvro22@outlook.com>
2020-02-16 15:03:26 -0800

    2.1. Adding GameManager Script to spawn player and camera

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject spawnerCanvas;
+    public GameObject playerCamera;
+    public Button spawnBtn;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spawnerCanvas.SetActive(true);
+        spawnBtn.onClick.AddListener(SpawnPlayerWithCam);
+    }
+
+    public void SpawnPlayerWithCam()
+    {
+        float randomPos = Random.Range(-5, 5);
+        var position = playerPrefab.transform.position;
+        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(randomPos*position.x, position.y),
+            Quaternion.identity);
+
+        
+        var spawnedPos = player.transform.position;
+        GameObject cam = PhotonNetwork.Instantiate(playerCamera.name, new Vector3(spawnedPos.x, spawnedPos.y,-10),
+            Quaternion.identity);
+
+        player.GetComponent<Player>().cam = cam.GetComponent<Camera>();
+        cam.GetComponent<CameraMove>()._target = player.transform;
+
+        spawnerCanvas.SetActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
